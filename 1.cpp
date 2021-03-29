f4806455f43986732a5bb6022ed5decba0f31f89
kaineomg <mgt62rus@gmail.com>
2021-03-29 19:31:20 +0300

    first commit

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+int main()
+{
+	int size = 10;
+	
+	char* expr = new char[size];
+	cin >> expr;
+	int n = strlen(expr);
+	cout << n << endl;
+	for (int i = 0;i <n;i++)
+	{
+		if (expr[i] == '(') 
+		{
+
+		}
+	}
+	
+}
