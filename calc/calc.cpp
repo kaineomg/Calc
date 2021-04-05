07f364f877357390acc1cb7cf28add7b9ac2eeec
kaineomg <mgt62rus@gmail.com>
2021-04-05 18:01:54 +0300

    po4ti vse

---
+++ calc/calc.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+float number()
+{
+    int res = 0;
+    while (true)
+    {
+        char c = cin.get();
+        if (c >= '0' && c <= '9')
+            res = res * 10 + c - '0';
+        else
+        {
+            cin.putback(c); //возвращает заданный символ назад во входной поток
+            return res;
+        }
+    }
+}
+
+float expr();
+
+float ckobki()
+{
+    char c = cin.get();
+    if (c == '(')
+    {
+        float x = expr();
+        cin.get();
+        return x;
+    }
+    else
+    {
+        cin.putback(c);
+        return number();
+    }
+}
+
+float factor()
+{
+    int x = ckobki();
+    while (true)
+    {
+        char c = cin.get();
+        switch (c)
+        {
+        case '*':
+            x *= ckobki();
+            break;
+        case '/':
+            x /= ckobki();
+            break;
+        default:
+            cin.putback(c);
+            return x;
+        }
+    }
+}
+
+float expr()
+{
+    int x = factor();
+    while (true)
+    {
+        char c = cin.get();
+        switch (c)
+        {
+        case '+':
+            x += factor();
+            break;
+        case '-':
+            x -= factor();
+            break;
+        default:
+            cin.putback(c);
+            return x;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Введите выражение: ";
+    float res = expr();
+    cout << "Ответ = " << res << endl;
+    return 0;
+}
